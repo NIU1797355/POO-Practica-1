c9d5a91c67eabb5237f587b6cdad116a9e3de3cc
Jiaxu <1797355@uab.cat>
2026-03-21 19:56:07 +0100

    modified main.py (add: Fabrica)

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import logging
 import math
+from date time import datetime
 
 # === Fabrica ===
 
 class Fabrica:
     def __init__(self, nom):
         self._nom = nom
-    def afegirModelCotxe(nom: str, es_electric: bool)
+    def afegir_model_cotxe(nom: str, es_electric: bool, cilindrada: int, num_portes: int, tipus_canvi: str, tipus_combustible: str): bool
+    pass
+    def afegir_model_moto(nom: str, es_electric: bool, cilindrada: int, tipus_rodes: str, carnet_necessari: str): bool
+    pass
+def produir_vehicle(model: ModelVehicle, color: str, data: Date): VehicleProduit
+    pass
