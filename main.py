a3b76ccdfc4e6dbb406b137477f68a121c1fff8a
Jiaxu <1797355@uab.cat>
2026-03-21 22:46:57 +0100

    fixed: main.py (syntax error)

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ class Fabrica:
     def __init__(self, nom):
         self._nom = nom
     def afegir_model_cotxe(nom: str, es_electric: bool, cilindrada: int, num_portes: int, tipus_canvi: str, tipus_combustible: str) -> bool:
-    pass
+        pass
     def afegir_model_moto(nom: str, es_electric: bool, cilindrada: int, tipus_rodes: str, carnet_necessari: str) -> bool:
-    pass
-def produir_vehicle(model: ModelVehicle, color: str, data: Date) -> VehicleProduit:
-    pass
+        pass
+    def produir_vehicle(model: ModelVehicle, color: str, data: Date) -> VehicleProduit:
+        pass
 
 
 
