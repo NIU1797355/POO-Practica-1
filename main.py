ece8094116f44617410bff12acffa4637f7d8b60
Jiaxu <1797355@uab.cat>
2026-03-21 22:32:02 +0100

    fixed: main.py (syntax error)

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from date time import datetime
 class Fabrica:
     def __init__(self, nom):
         self._nom = nom
-    def afegir_model_cotxe(nom: str, es_electric: bool, cilindrada: int, num_portes: int, tipus_canvi: str, tipus_combustible: str): bool
+    def afegir_model_cotxe(nom: str, es_electric: bool, cilindrada: int, num_portes: int, tipus_canvi: str, tipus_combustible: str) -> bool:
     pass
-    def afegir_model_moto(nom: str, es_electric: bool, cilindrada: int, tipus_rodes: str, carnet_necessari: str): bool
+    def afegir_model_moto(nom: str, es_electric: bool, cilindrada: int, tipus_rodes: str, carnet_necessari: str) -> bool:
     pass
-def produir_vehicle(model: ModelVehicle, color: str, data: Date): VehicleProduit
+def produir_vehicle(model: ModelVehicle, color: str, data: Date) -> VehicleProduit:
     pass
+
+
+
+# === Main ===
