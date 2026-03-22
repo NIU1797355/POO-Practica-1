9d65656f7e999c4d51434097daf043e881b1cf3b
Jiaxu <1797355@uab.cat>
2026-03-22 01:03:55 +0100

    fixed: main.py (datetime)

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from datetime import datetime
 class Fabrica:
     def __init__(self, nom):
         self._nom = nom
-    def afegir_model_cotxe(nom: str, es_electric: bool, cilindrada: int, num_portes: int, tipus_canvi: str, tipus_combustible: str) -> bool:
+        self._lines_produccio = None
+        self._inventari = None
+        self._registre = None
+    def afegirModelCotxe(self, nom: str, es_electric: bool, cilindrada: int, num_portes: int, tipus_canvi: str, tipus_combustible: str) -> bool:
         pass
-    def afegir_model_moto(nom: str, es_electric: bool, cilindrada: int, tipus_rodes: str, carnet_necessari: str) -> bool:
+    def afegirModelMoto(self, nom: str, es_electric: bool, cilindrada: int, tipus_rodes: str, carnet_necessari: str) -> bool:
         pass
-    def produir_vehicle(model: ModelVehicle, color: str, data: Date) -> VehicleProduit:
+    def produirVehicle(self, model: ModelVehicle, color: str, data: Date) -> VehicleProduit:
         pass
 
 
